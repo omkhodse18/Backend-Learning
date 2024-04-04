01851cf3869d0f7d3e0d8ecc2d71e99739b1e3b4
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-04 19:18:59 +0530

    Get by ID done

--- To-do app/controllers/getTodos.js
+++ To-do app/controllers/getTodos.js
@@ -23,3 +23,36 @@ exports.getTodos = async(req,res) => {
         })
     }
 }
+
+
+// For getTodosById, you can also write a seperate controller
+exports.getTodosById = async(req, res) => {
+    try {
+        // Extract todo items basis on ID
+        const id = req.params.id;
+        const todo = await Todo.findById({_id: id});
+
+        //Data for given id is not found
+        if(!todo){
+            return res.status(404).json({
+                success:false,
+                message:"No data found for given id"
+            })
+        }
+        
+        res.status(200).json({
+            success:true,
+            data: todo,
+            message: `Data for ${id} found successfully`
+        })
+    } 
+    catch(error) {
+        console.error(error);
+
+        res.status(500).json({
+            success:false,
+            error : error.message,
+            message:"Internal server error"
+        })
+    }
+}
--- To-do app/routes/todos.js
+++ To-do app/routes/todos.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 //Importing controller
 const {createTodo} = require("../controllers/createTodo");
-const {getTodos} = require("../controllers/getTodos");
+const {getTodos, getTodosById} = require("../controllers/getTodos");
+const {updateTodo} = require("../controllers/updateTodo");
 
 // Defining API routes
 router.post("/createTodo", createTodo);
 
 // Get data
 router.get('/getTodos', getTodos)
+
+// Get by ID
+router.get('/getTodos/:id', getTodosById);
+
 
 module.exports = router;
