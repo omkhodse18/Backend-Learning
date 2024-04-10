682db70a071b3140dfece30e1421cb3cc8414bd7
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-11 00:45:37 +0530

    Setup done

--- 3 Login-signup/index.js
+++ 3 Login-signup/index.js
@@ -1,2 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+
+const dbConnect = require('./config/database');
+dbConnect();  
+
+//or
+// require('./config/database').dbConnect();
+
+const user = require('./routes/user');
+app.use('/api/v1',user);
+
+app.listen(PORT, (req,res) => {
+    console.log(`App running on port number ${PORT}`);
+})
