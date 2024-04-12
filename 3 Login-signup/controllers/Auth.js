bb9f09ba0c25de5f741654a370ff939864975d33
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-12 22:59:56 +0530

    Login Route Handler done

--- 3 Login-signup/controllers/Auth.js
+++ 3 Login-signup/controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/User.js");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //Signup route handler
 exports.signup = async(req,res) => {
@@ -47,5 +49,85 @@ exports.signup = async(req,res) => {
             success:false,
             message:"User cannot be registered, please try again letter"
         })
+    }
+}
+
+////// Login handler
+
+exports.login = async(req, res) => {
+
+    try{
+        // Extracting email and pass from login req body
+        const {email, password} = req.body;
+        
+        // If email or pass is empty
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the details carefully."
+            });
+        }
+
+        // Checking if user entry is in database
+        let user = await User.findOne({email});
+        //If not a registered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered."
+            });
+        }
+
+        const payload = {
+            email: user.email,
+            id : user._id,
+            role: user.role
+        }
+        // Password verification and Generating JWT token
+        if(await bcrypt.compare(password, user.password) ){
+            //Password match
+
+            // Creating JWT token using sign method
+            let token = jwt.sign(payload,
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn:"2hr",
+                                }
+            );
+            
+            user = user.toObject();
+
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expires:new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:"User logged in successfully"
+            })
+        }
+        else{
+            // Password do not match
+            return res.status(403).json({
+                success:false,
+                message:"Password does not match."
+            });
+        }
+
+
+    } 
+    catch(error) {
+        console.log(error);
+
+        return res.status(500).json({
+            success:false,
+            message:"Login failure"
+        });
     }
 }
