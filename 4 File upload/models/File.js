014e6dbc39f2328a15db5e3c15782b0cc8199efe
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-16 10:09:47 +0530

    Nodemailer - sending mail using gmail server - DONE | FileUpload

--- 4 File upload/models/File.js
+++ 4 File upload/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -19,9 +21,39 @@ const fileSchema = new mongoose.Schema({
 
 })
 
+// Post middleware
+// Here doc is entry which store in database
+fileSchema.post("save", async function(doc){
+    try {
+        console.log("DOC -> ",doc);
+
+        // transporter
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user : process.env.MAIL_USER,
+                pass : process.env.MAIL_PASS
+            }
+        })
+
+        // send mail
+        let info = await transporter.sendMail({
+            from: 'OmiKhodse',
+            to: doc.email,
+            subject: "New file uploaded on Cloudinary",
+            html: `<h1>File uploaded successfully</h1> View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>`
+        })
+
+        console.log("INFO -> ",info);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
 // module.exports = mongoose.model("File", fileSchema);
-
 // Another way
-
 const File = mongoose.model("File", fileSchema);
 module.exports = File
